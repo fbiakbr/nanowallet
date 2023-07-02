4bb26c0c46ea1f66b55ed2f46640e03bc145b47f
fbiakbr <febiakbar19@gmail.com>
2023-07-02 15:14:20 +0700

    db

--- database/db_smart.sql
+++ database/db_smart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2023 at 09:52 AM
+-- Generation Time: Jul 02, 2023 at 09:48 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -136,7 +136,73 @@ INSERT INTO `auth_logins` (`id`, `ip_address`, `email`, `user_id`, `date`, `succ
 (38, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-05-30 03:14:45', 1),
 (39, '127.0.0.1', 'admin', NULL, '2023-05-30 03:15:12', 0),
 (40, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-05-30 03:15:18', 1),
-(41, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-05-30 03:16:32', 1);
+(41, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-05-30 03:16:32', 1),
+(42, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-05-30 07:53:58', 1),
+(43, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-05-30 09:03:24', 1),
+(44, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-05-30 09:36:29', 1),
+(45, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-05-30 09:36:53', 1),
+(46, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-05-31 03:58:22', 1),
+(47, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-05-31 04:02:13', 1),
+(48, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-05-31 04:04:51', 1),
+(49, '::1', 'admin@smknutirto.sc.id', 1, '2023-05-31 04:26:50', 1),
+(50, '192.168.10.160', 'admin@smknutirto.sc.id', 1, '2023-05-31 05:00:51', 1),
+(51, '192.168.10.230', 'admin@smknutirto.sc.id', 1, '2023-05-31 05:07:21', 1),
+(52, '192.168.10.160', 'admin@smknutirto.sc.id', 1, '2023-05-31 05:12:20', 1),
+(53, '192.168.10.160', 'admin2@smknutirto.sch.id', 3, '2023-05-31 05:26:50', 1),
+(54, '192.168.10.160', 'admin@smknutirto.sc.id', 1, '2023-05-31 05:31:18', 1),
+(55, '192.168.10.230', 'admin2@smknutirto.sch.id', 3, '2023-05-31 06:02:35', 1),
+(56, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-05-31 06:37:48', 1),
+(57, '::1', 'admin2@smknutirto.sch.id', 3, '2023-05-31 06:38:41', 1),
+(58, '127.0.0.1', 'admin4', NULL, '2023-06-07 04:01:53', 0),
+(59, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-06-07 04:02:00', 1),
+(60, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-07 04:02:16', 1),
+(61, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-06-07 09:02:42', 1),
+(62, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-06-07 10:42:33', 1),
+(63, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-06-12 05:31:14', 1),
+(64, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-06-12 05:38:33', 1),
+(65, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-06-12 05:41:39', 1),
+(66, '127.0.0.1', 'admin2@smknutirto.sch.id', 3, '2023-06-12 07:29:42', 1),
+(67, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-06-12 10:39:30', 1),
+(68, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-06-12 11:31:28', 1),
+(69, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-06-12 11:33:00', 1),
+(70, '127.0.0.1', 'admin@smknutirto.sc.id', 1, '2023-06-12 22:40:53', 1),
+(71, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-12 23:38:13', 1),
+(72, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 06:05:07', 1),
+(73, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 11:50:09', 1),
+(74, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 12:43:17', 1),
+(75, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 12:48:51', 1),
+(76, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 13:01:53', 1),
+(77, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 13:02:16', 1),
+(78, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 13:04:46', 1),
+(79, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 13:05:04', 1),
+(80, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 14:22:08', 1),
+(81, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 14:23:05', 1),
+(82, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 14:27:57', 1),
+(83, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-13 14:33:35', 1),
+(84, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-14 00:09:36', 1),
+(85, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-20 15:06:11', 1),
+(86, '::1', 'admin2@smknutirto.sch.id', 3, '2023-06-20 15:09:47', 1),
+(87, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-20 15:19:14', 1),
+(88, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-20 16:20:12', 1),
+(89, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-21 04:13:25', 1),
+(90, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-21 04:56:24', 1),
+(91, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-21 04:58:29', 1),
+(92, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-21 05:00:16', 1),
+(93, '192.168.10.155', 'admin@smknutirto.sc.id', 1, '2023-06-21 05:32:24', 1),
+(94, '192.168.10.155', 'admin@smknutirto.sc.id', 1, '2023-06-21 05:32:49', 1),
+(95, '192.168.10.155', 'admin@smknutirto.sc.id', 1, '2023-06-21 05:35:42', 1),
+(96, '192.168.10.155', 'admin@smknutirto.sc.id', 1, '2023-06-21 05:38:05', 1),
+(97, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-24 06:32:28', 1),
+(98, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-24 06:34:22', 1),
+(99, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-24 06:35:49', 1),
+(100, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-24 06:45:52', 1),
+(101, '::1', 'admin', NULL, '2023-06-24 11:45:07', 0),
+(102, '::1', 'admin@smknutirto.sc.id', 1, '2023-06-24 11:45:18', 1),
+(103, '::1', 'admin@smknutirto.sc.id', 1, '2023-07-01 10:14:34', 1),
+(104, '::1', 'admin@smknutirto.sc.id', 1, '2023-07-01 10:15:23', 1),
+(105, '::1', 'admin@smknutirto.sc.id', 1, '2023-07-01 10:17:18', 1),
+(106, '::1', 'admin', NULL, '2023-07-02 07:45:58', 0),
+(107, '::1', 'admin@smknutirto.sc.id', 1, '2023-07-02 07:46:02', 1);
 
 -- --------------------------------------------------------
 
@@ -265,10 +331,19 @@ CREATE TABLE `pemasukan` (
 --
 
 INSERT INTO `pemasukan` (`id_pemasukan`, `tgl_pemasukan`, `jam`, `nis`, `nama_siswa`, `kelas`, `jumlah`, `keterangan`) VALUES
-(33, '2023-05-29', '20:47:27', '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 9500000, ''),
-(34, '2023-05-29', '20:47:55', '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 3200000, ''),
-(35, '2023-04-30', '08:00:46', '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 12000000, ''),
-(36, '2023-05-30', '02:03:23', '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 30000, '');
+(45, '2023-05-31', '13:05:04', '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 150000, ''),
+(46, '2023-05-31', '13:40:37', '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 150000, ''),
+(47, '2023-05-31', '13:43:34', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 50000, ''),
+(48, '2023-05-31', '14:10:29', '6289', 'FATKHUL MANAN', 'XI TJKT 1', 10000, ''),
+(49, '2023-06-07', '11:02:39', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 5000000, ''),
+(50, '2023-06-12', '12:38:54', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 50000, ''),
+(51, '2023-06-12', '12:40:56', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 280000, ''),
+(52, '2023-06-12', '14:29:56', '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 150000, ''),
+(54, '2023-06-14', '07:47:42', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 100000, 'ISI SALDO'),
+(55, '2023-06-20', '22:16:00', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 100000, 'ISI SALDO'),
+(56, '2023-06-21', '12:12:54', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 100000, 'ISI SALDO'),
+(57, '2023-06-21', '12:38:21', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 50000, 'ISI SALDO'),
+(58, '2023-06-24', '13:36:29', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 50000, 'ISI SALDO');
 
 -- --------------------------------------------------------
 
@@ -295,11 +370,16 @@ CREATE TABLE `pembayaran` (
 --
 
 INSERT INTO `pembayaran` (`id_pembayaran`, `nis`, `nama_siswa`, `kelas`, `tagihan`, `bulan`, `tanggal_pembayaran`, `jam`, `jumlah_bayar`, `sisa_tagihan`, `status_pembayaran`) VALUES
-(52, '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 110000, 'JANUARI', '2023-05-30', '01:46:05', 110000, 0, 'LUNAS'),
-(53, '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 110000, 'FEBRUARI', '2023-05-30', '01:53:53', 110000, 0, 'LUNAS'),
-(54, '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 110000, 'APRIL', '2023-05-30', '01:54:23', 2900000, 0, 'LUNAS'),
-(55, '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 110000, 'MARET', '2023-05-30', '01:55:04', 110000, 0, 'LUNAS'),
-(56, '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 110000, 'JUNI', '2023-05-30', '01:57:47', 9400000, 0, 'LUNAS');
+(83, '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 110000, 'JANUARI', '2023-06-13', '20:02:04', 110000, 0, 'LUNAS'),
+(84, '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 110000, 'FEBRUARI', '2023-06-13', '20:04:57', 110000, 0, 'LUNAS'),
+(85, '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 110000, 'OKTOBER', '2023-06-20', '23:05:50', 110000, 0, 'LUNAS'),
+(86, '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 110000, 'NOVEMBER', '2023-06-20', '23:12:09', 110000, 0, 'LUNAS'),
+(87, '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 110000, 'MARET', '2023-06-21', '11:14:20', 110000, 0, 'LUNAS'),
+(88, '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 110000, 'MEI', '2023-06-21', '11:17:41', 110000, 0, 'LUNAS'),
+(89, '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 110000, 'JULI', '2023-06-21', '11:17:57', 110000, 0, 'LUNAS'),
+(90, '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 110000, 'OKTOBER', '2023-06-21', '11:20:38', 110000, 0, 'LUNAS'),
+(91, '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 110000, 'AGUSTUS', '2023-06-24', '13:35:01', 110000, 0, 'LUNAS'),
+(92, '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 110000, 'APRIL', '2023-06-24', '18:48:10', 110000, 0, 'LUNAS');
 
 -- --------------------------------------------------------
 
@@ -323,11 +403,20 @@ CREATE TABLE `pengeluaran` (
 --
 
 INSERT INTO `pengeluaran` (`id_pengeluaran`, `tgl_pengeluaran`, `jam`, `nis`, `nama_siswa`, `kelas`, `jumlah`, `keterangan`) VALUES
-(4, '2023-05-30', '01:46:05', '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 110000, 'PEMBAYARAN SPP'),
-(5, '2023-05-30', '01:53:53', '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 110000, 'PEMBAYARAN SPP'),
-(6, '2023-05-30', '01:54:23', '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 2900000, 'PEMBAYARAN SPP'),
-(7, '2023-05-30', '01:55:04', '6291', 'M. WILDAN ILMI', 'XI TJKT 1', 110000, 'PEMBAYARAN SPP'),
-(8, '2023-05-30', '01:57:47', '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 9400000, 'PEMBAYARAN SPP');
+(39, '2023-06-13', '20:02:04', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 110000, 'PEMBAYARAN SPP'),
+(40, '2023-06-13', '20:04:15', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 100000, 'PENARIKAN SALDO'),
+(41, '2023-06-13', '20:04:57', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 110000, 'PEMBAYARAN SPP'),
+(42, '2023-06-13', '20:07:55', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 500000, 'PENARIKAN SALDO'),
+(43, '2023-06-13', '21:25:46', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 1000000, 'PENARIKAN SALDO'),
+(47, '2023-06-20', '22:14:29', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 500000, 'PENARIKAN SALDO'),
+(48, '2023-06-20', '23:05:50', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 110000, 'PEMBAYARAN SPP'),
+(49, '2023-06-21', '11:17:57', '6290', 'M FADHIL FERDIANSYAH', 'XI TJKT 1', 110000, 'PEMBAYARAN SPP'),
+(50, '2023-06-21', '12:12:20', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 70000, 'PENARIKAN SALDO'),
+(51, '2023-06-21', '12:13:25', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 500000, 'PENARIKAN SALDO'),
+(52, '2023-06-21', '12:33:42', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 50000, 'PENARIKAN SALDO'),
+(53, '2023-06-24', '13:33:36', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 10000, 'PENARIKAN SALDO'),
+(54, '2023-06-24', '13:36:10', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 40000, 'PENARIKAN SALDO'),
+(55, '2023-07-01', '17:15:52', '6282', 'AHMAD AZRIL AFIF', 'XI TJKT 1', 100000, 'PENARIKAN SALDO');
 
 -- --------------------------------------------------------
 
@@ -346,10 +435,10 @@ CREATE TABLE `saldo` (
 --
 
 INSERT INTO `saldo` (`id_saldo`, `nis`, `saldo`) VALUES
-(1, '6291', 0),
-(2, '6290', 100000),
-(4, '6289', 0),
-(5, '6282', 0);
+(9, '6290', 80000),
+(10, '6291', 40000),
+(11, '6282', 100000),
+(12, '6289', 10000);
 
 -- --------------------------------------------------------
 
@@ -600,7 +689,7 @@ ALTER TABLE `auth_groups`
 -- AUTO_INCREMENT for table `auth_logins`
 --
 ALTER TABLE `auth_logins`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
 
 --
 -- AUTO_INCREMENT for table `auth_permissions`
@@ -636,25 +725,25 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `pemasukan`
 --
 ALTER TABLE `pemasukan`
-  MODIFY `id_pemasukan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_pemasukan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 
 --
 -- AUTO_INCREMENT for table `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
-  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT for table `saldo`
 --
 ALTER TABLE `saldo`
-  MODIFY `id_saldo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_saldo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `users`
